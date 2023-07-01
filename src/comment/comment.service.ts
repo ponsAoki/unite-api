3e9b6e96e38c7b59b0c114df3b87ad751fc2e469
“ponsAoki” <aokishogo20pk@gmail.com>
2023-07-01 17:18:50 +0900

    fix: fix check for my comment policy

--- src/comment/comment.service.ts
+++ src/comment/comment.service.ts
@@ -6,30 +6,34 @@ import { UpdateCommentInput } from './dto/update-comment-input';
 
 @Injectable()
 export class CommentService {
-  constructor(
-    private readonly prismaService: PrismaService
-  ) {}
+  constructor(private readonly prismaService: PrismaService) {}
 
   //一件取得
   findOne(id) {
-    return this.prismaService.comment.findUnique({ where: { id } })
+    return this.prismaService.comment.findUnique({ where: { id } });
+  }
+
+  //同じproductIdを持つcommentを全権取得
+  findManyByProductId(productId: string) {
+    return this.prismaService.comment.findMany({
+      where: { productId },
+    });
   }
 
   //コメント作成
   create(input: CreateCommentInput): PrismaPromise<Comment> {
-    return this.prismaService.comment.create({ data: input })
+    return this.prismaService.comment.create({ data: input });
   }
   //コメントの編集
   update(id: string, input: UpdateCommentInput): PrismaPromise<Comment> {
     return this.prismaService.comment.update({
       where: { id },
       data: input,
-    })
+    });
   }
 
   //コメントの削除
   delete(id: string) {
     return this.prismaService.comment.delete({ where: { id } });
   }
-
 }
