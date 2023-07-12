1b1deb0ea0103015d0ed4330bcbecd7d3ef6c085
nagayamajun <85071324+nagayamajun@users.noreply.github.com>
2023-07-13 00:18:03 +0900

    修正

--- src/common/decorators/employeeAuth.decorator.ts
+++ src/common/decorators/employeeAuth.decorator.ts
@@ -1,5 +1,6 @@
 import { ExecutionContext, createParamDecorator } from "@nestjs/common";
 
+//企業側のカスタムデコレーター
 export const FirebaseAuth = createParamDecorator(
   (data: unknown, ctx: ExecutionContext) => {
     const req = ctx.switchToHttp().getRequest();
