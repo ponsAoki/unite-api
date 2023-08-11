a1cff5fa817e7c577ee2092a1d42915be7f84937
“ponsAoki” <aokishogo20pk@gmail.com>
2023-08-12 00:41:43 +0900

    feat: applicantIdとrecruitIdから応募情報を取得できるapi追加

--- test/fixture/product.ts
+++ test/fixture/product.ts
@@ -1,19 +1,22 @@
-import { Product } from "@prisma/client";
+import { Product } from '@prisma/client';
 
 export class TestProduct {
-  create(num=10): Product[] {
+  create(num = 10): Product[] {
     return [...new Array(num)].map((_, n) => {
       const t = new Date();
       t.setSeconds(t.getSeconds() - num + n);
       return {
         id: `productId${n}`,
         recruitId: `userRecruitId${n}`,
-        headline: `headline${n}`,
+        name: `name${n}`,
+        skills: ['HTML', 'CSS', 'JAVA_SCRIPT'],
+        reasonForSkillSelection: `reasonForSkillSelection${n}`,
+        developmentBackground: `developmentBackground${n}`,
+        overview: `overview${n}`,
         url: `url${n}`,
-        detail: `detail${n}`,
         createdAt: t,
-        updatedAt: t
-      }
-    })
+        updatedAt: t,
+      };
+    });
   }
 }
